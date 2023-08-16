8f4be8fc51c258ef5853e94d73ed810ac90ecc28
bryan-tt <bryantsan916@gmail.com>
2023-08-15 23:10:41 -0700

    Updating branch

---
+++ frontend/app.py
@@ -0,0 +1,116 @@
+import psycopg2
+import json
+from flask import Flask, jsonify, render_template
+from datetime import datetime
+import sys
+import os
+
+#################################################
+# Database Setup
+#################################################
+
+conn = psycopg2.connect(database="proj_4",
+                            user="postgres",
+                            password="postgres",
+                            host="localhost",
+                            port="5432")
+cur = conn.cursor()
+
+
+
+#################################################
+# Flask Setup
+#################################################
+
+app = Flask(__name__)
+
+################################################
+# Flask Routes
+#################################################
+
+@app.route('/')
+def index():
+    return render_template('index.html') #Home page that has search engine/preferences and rotating images
+
+@app.route('/team')
+def team():
+    return render_template('team.html') 
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/results')
+def results():
+    # Simulated results from the database...to be replaced by backend group work querying database
+    ##### Sample Code ######
+    results = [
+        {'image': 'model_s.jpg', 'price': 100000, 'year': 2018, 'manufacturer': 'Tesla', 'model': 'Model S', 'id': 1, 'size': 'Any', 'condition': 'Any'},
+        {'image': 'model_x.jpg', 'price': 50000, 'year': 2017, 'manufacturer': 'Tesla', 'model': 'Model X', 'id': 2, 'size': 'Any', 'condition': 'Any'},
+        {'image': 'toyota.jpg', 'price': 75000, 'year': 2016, 'manufacturer': 'Toyota', 'model': 'Corrola', 'id': 3, 'size': 'Any', 'condition': 'Any'},
+    ]
+
+    reccomendations = [
+         {'image': 'audi7.jpg', 'price': 100000, 'year': 2018, 'manufacturer': 'Audi', 'model': 'A7', 'id': 4, 'size': 'Any', 'condition': 'Any'},
+        {'image': 'gwagon.jpg', 'price': 50000, 'year': 2017, 'manufacturer': 'Mercedez', 'model': 'G Wagon', 'id': 5, 'size': 'Any', 'condition': 'Any'},
+        {'image': 'vwcc.jpg', 'price': 75000, 'year': 2016, 'manufacturer': 'Volkswagen', 'model': 'CC', 'id': 6, 'size': 'Any', 'condition': 'Any'},
+         {'image': 'audi7.jpg', 'price': 100000, 'year': 2018, 'manufacturer': 'Audi', 'model': 'A7', 'id': 4, 'size': 'Any', 'condition': 'Any'},
+        {'image': 'gwagon.jpg', 'price': 50000, 'year': 2017, 'manufacturer': 'Mercedez', 'model': 'G Wagon', 'id': 5, 'size': 'Any', 'condition': 'Any'},
+        {'image': 'vwcc.jpg', 'price': 75000, 'year': 2016, 'manufacturer': 'Volkswagen', 'model': 'CC', 'id': 6, 'size': 'Any', 'condition': 'Any'},
+    ]
+
+    ##########################
+
+
+    ########### Actual Code ################
+
+    #Something Like
+    """
+    cursor.execute(select * from used_cars where price<20000 and price>100000 and 'year' > 2012)
+    rows=cursor.fetchall()
+    rows=pd.DataFrame(rows).tojson()
+    return rows
+    
+
+    """
+
+    #########################################
+
+
+
+    return render_template('results.html', results=results, reccomendations=reccomendations) #Webpage that gets results following search
+
+@app.route('/car/<int:car_id>') #'/<int:car_id>'
+def car_details(car_id):
+    # car = get_car_details(car_id) #Python functions that queries database for specific car and returns the details. Car_id=unique id of car in database
+    # prediction = get_price_prediction(car_id)
+    
+    ########### Sample Code #########
+    results = [
+        {'image': 'model_s.jpg', 'price': 100000, 'year': 2018, 'make': 'Tesla', 'model': 'Model S', 'id': 1},
+        {'image': 'model_x.jpg', 'price': 50000, 'year': 2017, 'make': 'Tesla', 'model': 'Model X', 'id': 2},
+        {'image': 'toyota.jpg', 'price': 75000, 'year': 2016, 'make': 'Toyota', 'model': 'Corrola', 'id': 3},
+    ]
+
+    for val in results:
+            if val['id']==car_id:
+                car=val
+    #################################
+
+
+    ###### ----- Actual code ------ #############
+    
+    #Something Like
+    """
+    cursor.execute(Select * from used_cars where id = car_id returning blah blah)
+    rows=cursor.fetchall()
+    rows=pd.DataFrame(rows).todict()/json
+    car=rows
+    """
+    #################################
+
+    return render_template('car.html', car = car)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ frontend/static/main.js
@@ -0,0 +1,132 @@
+//main.js
+
+//Helper Functions: 
+
+// Function to generate ranges and populate selectbox for nums
+function generateOptions(start, end, increment) {
+    var options = [];
+    for (let i = start; i <= end; i += increment) {
+        options.push(`<option value="${i}">${i}</option>`);
+    }
+    options.reverse()
+    return options.join('');
+}
+
+// Same function but for string values
+function generateStringOptions (array) {
+    var options=[]
+    for (let choice of array) {
+        options.push(`<option value= "${choice}">${choice}</option>`)
+    }
+    return options.join('')
+}
+
+
+///////////////////////////////////////
+
+
+
+// Populating initial values
+document.getElementById('maxPriceRange').innerHTML = generateOptions(5000, 200000, 5000)
+
+let conditionsArray = ['any','good', 'excellent', 'fair', 'like new', 'new', 'salvage']
+let newConditionsArray = conditionsArray.map(function(x) { return x.charAt(0).toUpperCase() + x.slice(1,)})
+document.getElementById('condition').innerHTML = generateStringOptions(newConditionsArray)
+
+
+let stateArray = ['any','az', 'ar', 'fl', 'ma', 'nc', 'ny', 'or', 'pa', 'tx', 'wa', 'wi',
+'al', 'ak', 'ca', 'co', 'ct', 'dc', 'de', 'ga', 'hi', 'id', 'il',
+'in', 'ia', 'ks', 'ky', 'la', 'me', 'md', 'mi', 'mn', 'ms', 'mo',
+'mt', 'ne', 'nv', 'nj', 'nm', 'nh', 'nd', 'oh', 'ok', 'ri', 'sc',
+'sd', 'tn', 'ut', 'vt', 'va', 'wv', 'wy']
+let newStateArray = stateArray.map(function(x){return x.toUpperCase()})
+document.getElementById('state').innerHTML=generateStringOptions(newStateArray)
+
+
+let brandArray = ['any', 'gmc', 'chevrolet', 'toyota', 'ford', 'jeep', 'nissan', 'ram',
+'mazda', 'cadillac', 'honda', 'dodge', 'lexus', 'jaguar', 'buick',
+'chrysler', 'volvo', 'audi', 'infiniti', 'lincoln', 'alfa-romeo',
+'subaru', 'acura', 'hyundai', 'mercedes-benz', 'bmw', 'mitsubishi',
+'volkswagen', 'porsche', 'kia', 'rover', 'ferrari', 'mini',
+'pontiac', 'fiat', 'tesla', 'saturn', 'mercury', 'harley-davidson',
+'datsun', 'aston-martin', 'land rover', 'morgan']
+let newBrands = brandArray.map(function(x) { return x.charAt(0).toUpperCase() + x.slice(1,)})
+document.getElementById('manufacturer').innerHTML = generateStringOptions(newBrands)
+
+let size = ['any', 'full-size', 'mid-size', 'compact', 'sub-compact']
+let newSize = size.map(function(x) { return x.charAt(0).toUpperCase() + x.slice(1,)})
+document.getElementById('size').innerHTML = generateStringOptions(newSize)
+
+// Potentially remove 'any'
+let transmissionArray = ['any', 'automatic', 'manual']
+let newTransmissionArray = transmissionArray.map(function(x) {return x.charAt(0).toUpperCase() + x.slice(1,)})
+document.getElementById('transmission').innerHTML = generateStringOptions(newTransmissionArray)
+
+let odometerAraray = ['Less than 200000', 'Less than 400000', 'Less than 600000', 'Less than 800000']
+document.getElementById('odometer').innerHTML = generateStringOptions(odometerAraray)
+
+let cylinderArray = ['Any', '8 cylinders', '6 cylinders', '4 cylinders', '5 cylinders', '3 cylinders', '10 cylinders', '12 cylinders']
+document.getElementById('cylinder').innerHTML = generateStringOptions(cylinderArray)
+
+// Populates the text Value upon change
+// document.getElementById('maxPriceRange').addEventListener('input', function() {
+//     document.getElementById('maxPriceRangeValue').textContent = this.value;});
+
+///////////////
+
+
+
+// Hit Search button based on query options
+function searchCars () {
+    var query = document.getElementById('searchBar').value;
+    var priceMax = document.getElementById('maxPriceRange').value;
+    var condition = document.getElementById('condition').value;
+    var manufacturer = document.getElementById('manufacturer').value;
+    var size = document.getElementById('size').value;
+    var odometer = document.getElementById('odometer').value;
+    var state = document.getElementById('state').value;
+    var transmission = document.getElementById('transmission').value;
+    var cylinders = document.getElementById('cylinder').value;
+
+
+    //Redirect to results after search is made
+    window.location.href = "/results?query=" + query + "&maxPrice=" + priceMax + "&condition=" + condition + "&state" + state + "&manufacturer=" + manufacturer + "&size=" + size + "&ododmeter=" + odometer + "&transmission=" + transmission + "&cylinders=" + cylinders;
+}
+
+// Navigate to pages Buttons
+function team(){
+    window.location.href = "/team"
+}
+
+function about(){
+    window.location.href = "/about"
+}
+
+//Sign Up Notification
+document.getElementById('notificationForm').addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const email = document.getElementById('formEmail').value
+    //Build Backend API to send email
+
+
+    //Send Alert for email
+    $('#notificationModal').modal('hide');
+    alert(`Thank you for signing up! Updates will be sent to ${email}`)
+})
+
+function scrollToNavBar(event){
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: $("#header").offset().top()
+    }, 1000)
+    // const navbar = document.getElementById('header')
+    // navbar.scrollIntoView({behavior: "smooth" })
+}
+
+
+//Hitting enter for search bar
+document.getElementById('searchForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    searchCars()
+})
